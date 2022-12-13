a9f660a39629f72dfc8853ed789a466d28cbaae5
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-12-12 21:05:05 -0300

    Primeiro Componente #01

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import Primeiro from "./components/basicos/Primeiro";
 
 const el = document.getElementById("root");
 
-const tag = <strong>Olá React!</strong>;
-
-ReactDOM.render(<div>{tag}</div>, el);
+ReactDOM.render(
+  <div>
+    <Primeiro />
+  </div>,
+  el
+);
