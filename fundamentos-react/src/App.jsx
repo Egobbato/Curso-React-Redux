43d12a1876bfa33357244c83b69f2d5367a5860d
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-12-15 17:06:52 -0300

    Componente Card #01

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -3,15 +3,27 @@ import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
 import Aleatorio from "./components/basicos/Aleatorio";
+import Card from "./components/layout/Card";
 
 export default function App(props) {
   return (
     <div id="app">
       <h1>Fundamentos React</h1>
-      <Aleatorio min={1} max={60} />
-      <Fragmento />
-      <ComParametro Titulo="Situação do Aluno" aluno="Eduardo" nota={9.3} />
-      <Primeiro />
+      <Card titulo="#04 - Desafio Aleatório">
+        <Aleatorio min={1} max={60} />
+      </Card>
+
+      <Card titulo="#03 - Fragmento">
+        <Fragmento />
+      </Card>
+
+      <Card titulo="#02 - Com Parametro">
+        <ComParametro Titulo="Situação do Aluno" aluno="Eduardo" nota={9.3} />
+      </Card>
+
+      <Card titulo="#01 - Primeiro Componente">
+        <Primeiro />
+      </Card>
     </div>
   );
 }
