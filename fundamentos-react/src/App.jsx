a2c2a45137a20e670f4a9537144be22ae704be71
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-12-15 16:19:39 -0300

    Desafio Número Aleatório

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import Primeiro from "./components/basicos/Primeiro";
 import ComParametro from "./components/basicos/ComParametro";
 import Fragmento from "./components/basicos/Fragmento";
+import Aleatorio from "./components/basicos/Aleatorio";
 
 export default function App(props) {
   return (
     <div id="app">
       <h1>Fundamentos React</h1>
+      <Aleatorio />
       <Fragmento />
       <ComParametro Titulo="Situação do Aluno" aluno="Eduardo" nota={9.3} />
       <Primeiro />
---
+++ fundamentos-react/src/components/basicos/Aleatorio.jsx
@@ -0,0 +1,3 @@
+export default function Aleatorio(props) {
+  return Math.floor(Math.random() * 10);
+}
